ecd133d121f11ecaf082f69b42ec43ec646b1f85
Adam William Weber <awweber@eceLinux2.uwaterloo.ca>
2011-06-25 16:01:47 -0400

    Adding some files that were missed

---
+++ p2b/process_init.c
@@ -0,0 +1,11 @@
+#include "process_init.h"
+
+const struct s_pcb_init process_init_table [] =
+{
+	{NULL_PROC_ID, 4, 1, 1024, null_process},
+	{TEST_PROC_1_ID, 2, 1, 1024, test_proc_1},
+	{TEST_PROC_2_ID, 2, 1, 1024, test_proc_2},
+	{TEST_PROC_3_ID, 2, 1, 1024, test_proc_3},
+	{TEST_PROC_4_ID, 2, 1, 1024, test_proc_4},
+	{TEST_PROC_5_ID, 2, 1, 1024, test_proc_5}
+};
